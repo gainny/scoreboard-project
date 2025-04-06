4aff5e3ffd4c72bc1f62b61fc8d1e1048401463a
nicamachii <159938956+gainny@users.noreply.github.com>
2025-04-06 23:32:25 +0900

    First Commit!

---
+++ index.js
@@ -0,0 +1,35 @@
+let home = 0
+let guest = 0
+
+
+function add_home1(){
+    home += 1
+    document.getElementById("score1").textContent = home
+}
+function add_home2(){
+    home += 2
+    document.getElementById("score1").textContent = home
+}
+function add_home3(){
+    home += 3
+    document.getElementById("score1").textContent = home
+}
+function add_guest1(){
+    guest += 1
+    document.getElementById("score2").textContent = guest
+}
+function add_guest2(){
+    guest += 2
+    document.getElementById("score2").textContent = guest}
+function add_guest3(){
+    guest += 3
+    console.log(guest)
+}
+
+function add_to_home(){
+    
+}
+document.getElementById("score1")
+
+
+
